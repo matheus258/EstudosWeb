5ddab8037094385a92fa7a996c8e0c047816d3cd
Matheus <matheusmarquesaraujo53@gmail.com>
2023-08-09 01:17:42 -0300

    Estudos Js de Tipos de Dados

---
+++ HoraDeCodar/javaScript/02_dadosOperadores/index.js
@@ -0,0 +1,18 @@
+// O que sao os tipos de dados?
+
+// Number
+    // Exemplos do tipo Number
+    const num = 1; // Numero interiro
+    const num1 = 1.5; // Numero Decimal
+    const num2 = -5 // Numero Negativo
+
+    console.log(typeof num, typeof num1, typeof num2);
+
+// String 
+    const name = 'José';
+    const sobreNome = 'Anderson';
+
+//Bolean
+// const 
+
+// Empty Values (Null, undefined)
