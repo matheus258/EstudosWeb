e98ed4d122c4aa9aa16c9b1b2f1192c9b0510881
matheus258 <matheus_marques258@outlook.com>
2023-09-12 10:39:38 -0300

    Estudos de DOM js

--- DIO/03_paginaJavascript/05_protocolo_http/assets/js/main.js
+++ DIO/03_paginaJavascript/05_protocolo_http/assets/js/main.js
@@ -3,6 +3,13 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-fetch(url).then(function (response) {
-    console.log(response);
-});
+fetch(url)
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function(err){
+        console.error('Error:', err); // Error: TypeError: Failed to fetch
+    })
+    .finally(function () {
+        console.info("Requisição Concluída");
+    })
---
+++ Estudos_Js/objects/index.js
@@ -0,0 +1,28 @@
+const Pessoa = function (nome, sobrenome, idade){
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.idade = idade;
+}
+
+const carla = new Pessoa ('Carla', 'Alcantara', 25)
+
+// document.write(carla.nome, ' ', carla.idade);
+
+// DOM
+
+let x = 0;
+
+function getPrincipal(){
+    // Muda o p toda vez que da click pelo ID;
+    let text = document.getElementById('principal').innerHTML = x % 2 == 0 ? 'Mudou o Texto' : 'Mudou novamente';
+    // Muda o p toda vez que da click pela Class
+    let turno = document.getElementsByClassName('secundario')[0].innerHTML = x % 2 == 0 ? 'Boa tarde' : 'Boa noite';
+    document.getElementsByClassName('secundario')[1].innerHTML = x % 2 == 0 ? 'Envio' : 'Retorno';
+    x++;
+}
+
+let text = document.getElementById('principal');
+let turno = document.getElementsByClassName('secundario')[0];
+
+
+console.log(text, turno);
