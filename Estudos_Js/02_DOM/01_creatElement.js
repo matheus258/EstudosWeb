b80073abd619c4350031c5cb36497f6bd408b8fd
matheus258 <matheus_marques258@outlook.com>
2023-09-20 21:12:58 -0300

    Estudos Js createElement

--- Estudos_Js/02_DOM/01_creatElement.js
+++ Estudos_Js/02_DOM/01_creatElement.js
@@ -8,15 +8,11 @@ console.log(novoParagrafo);
 
 let body = document.querySelector('body');
 
-console.log(body)
-
 body.appendChild(novoParagrafo);
 
 // Fazendo testes de add elementos
 
 var titlePrincipal = document.getElementsByClassName('title')[0];
-
-console.log(titlePrincipal);
 
 var el = document.createElement('H1');
 
@@ -34,3 +30,18 @@ let p = document.createElement('p');
 p.appendChild(document.createTextNode('Este é um span criado com javascript'));
 
 span.appendChild(p);
+
+// Terceiro teste, Criando um H2
+
+let subtitle = document.querySelector('.subtitle');
+
+let sub = document.createElement('H2');
+sub.appendChild(document.createTextNode('lorem lorem e lorem lorem ahfafafj  afpj a lorem.'));
+subtitle.appendChild(sub);
+
+// MUSSUM
+let mussum = document.querySelector('.mussum');
+let paragrafo = document.createElement('p');
+
+paragrafo.appendChild(document.createTextNode('Mussum Ipsum, cacilds vidis litro abertis.  Quem num gosta di mé, boa gentis num é. Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis. Manduma pindureta quium dia nois paga. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio.'));
+mussum.appendChild(paragrafo);
