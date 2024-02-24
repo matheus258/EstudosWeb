a6d2073d2d9c204d6534f317f1812381dbb1c33b
Matheus <matheusmarquesaraujo53@gmail.com>
2024-02-24 00:20:43 -0300

    Estudos Dev Web com Js

---
+++ Estudos_Js/02_DOM/02_removeChild.js
@@ -0,0 +1,11 @@
+// Remover elemento filho
+
+let lista = document.querySelector('.list');
+var ol = document.querySelector('.list ol');
+console.log(lista);
+lista.removeChild(ol);
+
+// Remover elemento
+
+let lorem = document.querySelector('.lorem');
+lorem.remove();
---
+++ Estudos_Js/02_DOM/03_adicionarElemento.js
@@ -0,0 +1,2 @@
+// Criar elemento
+// aula 40 https://www.youtube.com/watch?v=0XJyTa6B-GA&list=PLnDvRpP8BneysKU8KivhnrVaKpILD3gZ6&index=41&ab_channel=MatheusBattisti-HoradeCodar
